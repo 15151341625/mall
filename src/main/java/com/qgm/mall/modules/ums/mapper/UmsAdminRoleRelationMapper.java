51d2be5d2b85cc76f88dfb5e55e61d1fc036a773
qgm <1050680368@qq.com>
2021-01-11 17:57:56 +0800

    脚手架搭建

---
+++ src/main/java/com/qgm/mall/modules/ums/mapper/UmsAdminRoleRelationMapper.java
@@ -0,0 +1,19 @@
+package com.qgm.mall.modules.ums.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.qgm.mall.modules.ums.model.UmsAdminRoleRelation;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * <p>
+ * 后台用户和角色关系表 Mapper 接口
+ * </p>
+ *
+ * @author macro
+ * @since 2020-08-21
+ */
+public interface UmsAdminRoleRelationMapper extends BaseMapper<UmsAdminRoleRelation> {
+
+}
